76d34503bbd5bae486e5385aee626fcc14a8502b
keiukunnarpuu <annaliisa.tamme@voco.ee>
2025-05-27 22:36:51 +0300

    Created new article

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ sequelize
 const articleRouter = require('./routes/article');
 app.use('/', articleRouter);
 app.use('/article', articleRouter);
+app.use('admin/article', articleRouter)
 
 const authorRouter = require('./routes/author');
 app.use('/author', authorRouter);
--- routes/article.js
+++ routes/article.js
@@ -3,10 +3,13 @@ const express = require('express');
 const router = express.Router();
 // define article controller and export it for this file
 const articleController = require('../controllers/article');
+const articleAdminController = require('../controllers/admin/articles')
 
 // use controller functions according to the route
 router.get('/', articleController.getAllArticles);
 router.get('/article/:slug', articleController.getArticleBySlug);
+
+router.post('/admin/article/create', articleAdminController.createArticle);
 
 // export article router for using in default application file
 module.exports = router;
