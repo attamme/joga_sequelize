c0dd79390dd3086fe92034cf017ec8b372aa388d
keiukunnarpuu <annaliisa.tamme@voco.ee>
2025-05-27 20:26:19 +0300

    Getted article data by slug

--- controllers/article.js
+++ controllers/article.js
@@ -17,10 +17,27 @@ const getAllArticles =  (req, res) => {
         });
 };
 
+// show article by this slug
+const getArticleBySlug = (req, res) => {
+    const slug = req.params.slug;
+    Article.findOne({ 
+        where: { 
+            slug : req.params.slug 
+        } 
+    })
+    .then(article => {
+        console.log(article) 
+        return res.status(200).json({ article });
+    })
+    .catch(error => {
+        return res.status(500).send(error.message);
+    })
+}
+
 // export controller functions
 module.exports = {
-    getAllArticles
-    // getArticleBySlug: (req, res) => {
+    getAllArticles,
+    getArticleBySlug//: (req, res) => {
     //     const slug = req.params.slug;
     //     const article = Article.findOne({ where: { slug } });
     //     res.json(article);
--- index.js
+++ index.js
@@ -21,6 +21,7 @@ sequelize
 // using routes and controllers
 const articleRouter = require('./routes/article');
 app.use('/', articleRouter);
+app.use('/article', articleRouter);
 
 app.get('/', (req, res) => {
     res.json({ message: "Welcome tto sequelize application" })
